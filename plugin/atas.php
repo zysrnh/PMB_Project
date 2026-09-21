ae4e273f3a055b92d537cde7498bd22ceb835f6d
Naooo <naooolaf@gmail.com>
2026-09-21 18:10:33 +0700

    fix(header): selaraskan warna forest green, gold, dan flat design pada top bar & mobile header

--- plugin/atas.php
+++ plugin/atas.php
@@ -8,8 +8,32 @@ while ($data = $koneksi_db->sql_fetchrow($hasil)) {
     $id = md5($data['id']);
 
  echo '   
-<div class="kingster-mobile-header-wrap" ><div class="kingster-top-bar" style="background:#0b4d3c; padding: 6px 0;"><div class="kingster-top-bar-background" ></div><div class="kingster-top-bar-container kingster-container " ><div class="kingster-top-bar-container-inner clearfix" ><div class="kingster-top-bar-right kingster-item-pdlr"><ul id="kingster-top-bar-menu" class="sf-menu kingster-top-bar-menu kingster-top-bar-right-menu">
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9306 kingster-normal-menu" style="color: #ecfdf5;"><i class="fa fa-phone" style="color: #c89a3b;"></i> Telp. '.$data['telp'].' | </li>
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9306 kingster-normal-menu" style="color: #ecfdf5;"><i class="fa fa-envelope-o" style="color: #c89a3b;"></i> Email : '.$data['email'].'</li>
-</ul></div></div></div></div><div class="kingster-mobile-header kingster-header-background kingster-style-slide kingster-sticky-mobile-navigation " id="kingster-mobile-header" style="background: #ffffff; border-bottom: 1px solid #e2e8f0;"><div class="kingster-mobile-header-container kingster-container clearfix" ><div class="kingster-logo kingster-item-pdlr"><div class="kingster-logo-inner"><a class="" href="index.html" ><img src="images/'.$data['foto'].'" width="160" height="50" style="max-height: 48px; width: auto;" alt="'.$data['nama'].'" /></a></div></div>'; 	
+<div class="kingster-mobile-header-wrap" style="border-radius: 0px;">
+    <div class="kingster-top-bar" style="background:#0b4d3c !important; padding: 7px 10px; border-bottom: 1px solid rgba(255,255,255,0.1); border-radius: 0px;">
+        <div class="kingster-top-bar-background" style="background:#0b4d3c !important; display:none;"></div>
+        <div class="kingster-top-bar-container kingster-container">
+            <div class="kingster-top-bar-container-inner clearfix">
+                <div class="kingster-top-bar-right kingster-item-pdlr" style="text-align: center;">
+                    <ul id="kingster-top-bar-menu" class="sf-menu kingster-top-bar-menu kingster-top-bar-right-menu" style="display: flex; justify-content: center; flex-wrap: wrap; gap: 12px; margin: 0; padding: 0; list-style: none;">
+                        <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9306 kingster-normal-menu" style="color: #ecfdf5; font-size: 12px; font-weight: 500;">
+                            <i class="fa fa-phone" style="color: #c89a3b; margin-right: 4px;"></i> Telp. '.$data['telp'].'
+                        </li>
+                        <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9306 kingster-normal-menu" style="color: rgba(255,255,255,0.3); font-size: 12px;">|</li>
+                        <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9306 kingster-normal-menu" style="color: #ecfdf5; font-size: 12px; font-weight: 500;">
+                            <i class="fa fa-envelope-o" style="color: #c89a3b; margin-right: 4px;"></i> Email : '.$data['email'].'
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="kingster-mobile-header kingster-header-background kingster-style-slide kingster-sticky-mobile-navigation" id="kingster-mobile-header" style="background: #ffffff !important; border-bottom: 1px solid #e2e8f0; border-radius: 0px;">
+        <div class="kingster-mobile-header-container kingster-container clearfix">
+            <div class="kingster-logo kingster-item-pdlr">
+                <div class="kingster-logo-inner">
+                    <a class="" href="index.html">
+                        <img src="images/'.$data['foto'].'" width="160" height="50" style="max-height: 48px; width: auto;" alt="'.$data['nama'].'" />
+                    </a>
+                </div>
+            </div>'; 	
 } ?>
--- plugin/atas2.php
+++ plugin/atas2.php
@@ -8,13 +8,26 @@ while ($data = $koneksi_db->sql_fetchrow($hasil)) {
     $id = md5($data['id']);
 
  echo '   
-<div class="kingster-body-outer-wrapper " >
-<div class="kingster-body-wrapper clearfix kingster-with-frame">
-<div class="kingster-top-bar" style="background:#0b4d3c; border-bottom: 1px solid rgba(255,255,255,0.1); padding: 8px 0;"><div class="kingster-top-bar-background" style="background:#0b4d3c;"></div><div class="kingster-top-bar-container kingster-container " ><div class="kingster-top-bar-container-inner clearfix" ><div class="kingster-top-bar-right kingster-item-pdlr"><ul id="kingster-top-bar-menu" class="sf-menu kingster-top-bar-menu kingster-top-bar-right-menu">
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9306 kingster-normal-menu" style="color:#ecfdf5;"><i class="fa fa-phone" style="color:#c89a3b;"></i> Telp. '.$data['telp'].' | </li>
-<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9306 kingster-normal-menu" style="color:#ecfdf5;"><i class="fa fa-envelope-o" style="color:#c89a3b;"></i> Email : '.$data['email'].'</li>
-</ul></div></div></div></div>	
-<header class="kingster-header-wrap kingster-header-style-plain kingster-style-menu-right kingster-sticky-navigation kingster-style-fixed" data-navigation-offset="75px" style="background:#ffffff; border-bottom:1px solid #e2e8f0; box-shadow:0 1px 3px rgba(0,0,0,0.05);" >
+<div class="kingster-body-outer-wrapper" style="border-radius: 0px;">
+<div class="kingster-body-wrapper clearfix kingster-with-frame" style="border-radius: 0px;">
+<div class="kingster-top-bar" style="background:#0b4d3c !important; border-bottom: 1px solid rgba(255,255,255,0.1); padding: 8px 0; border-radius: 0px;">
+    <div class="kingster-top-bar-background" style="background:#0b4d3c !important; display:none;"></div>
+    <div class="kingster-top-bar-container kingster-container">
+        <div class="kingster-top-bar-container-inner clearfix">
+            <div class="kingster-top-bar-right kingster-item-pdlr">
+                <ul id="kingster-top-bar-menu" class="sf-menu kingster-top-bar-menu kingster-top-bar-right-menu">
+                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9306 kingster-normal-menu" style="color:#ecfdf5; font-size:13px; font-weight:500;">
+                        <i class="fa fa-phone" style="color:#c89a3b; margin-right:4px;"></i> Telp. '.$data['telp'].' &nbsp;|&nbsp; 
+                    </li>
+                    <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-9306 kingster-normal-menu" style="color:#ecfdf5; font-size:13px; font-weight:500;">
+                        <i class="fa fa-envelope-o" style="color:#c89a3b; margin-right:4px;"></i> Email : '.$data['email'].'
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>	
+<header class="kingster-header-wrap kingster-header-style-plain kingster-style-menu-right kingster-sticky-navigation kingster-style-fixed" data-navigation-offset="75px" style="background:#ffffff; border-bottom:1px solid #e2e8f0; box-shadow:0 1px 3px rgba(0,0,0,0.05); border-radius: 0px;" >
 <div class="kingster-header-background" style="background:#ffffff;"></div>
 <div class="kingster-header-container kingster-container">
 <div class="kingster-header-container-inner clearfix">
